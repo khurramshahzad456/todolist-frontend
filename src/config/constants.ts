e740c595c40897c7d852336a7e31854953976e2c
Khurram <khurramshahzad2398@gmail.com>
2024-04-08 23:59:21 +0500

    Update constants.ts

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,6 +1,6 @@
 import { storageService } from '../services/storage';
 
-export const BASE_URL = 'https://backend-kappa-three-72.vercel.app/';
+export const BASE_URL = 'https://todolist-backend-henna.vercel.app/';
 export const IsInDevelopment = process.env.NODE_ENV === 'development';
 export const TOKEN_KEY = 'todo-list-token';
 export const localStorageToken = storageService.getItem(TOKEN_KEY);
